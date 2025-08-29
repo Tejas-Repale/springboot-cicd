962d0bc01ce29306b93c6851b075c7297c9db484
Tejas-Repale <tejasrepale285@gmail.com>
2025-08-29 09:23:40 +0000

    first commit

---
+++ src/main/java/demo/example/demo/app/HelloController.java
@@ -0,0 +1,12 @@
+package demo.example.demo.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot on EC2!";
+    }
+}
---
+++ src/test/java/demo/example/demo/app/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.example.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
